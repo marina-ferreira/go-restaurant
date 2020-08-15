4a99e05edc6140bca8135ea0d70fa8200474675d
Marina Ferreira <marina.ferreira.developer@gmail.com>
2020-08-15 11:47:11 -0300

    Deletes food item

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -69,7 +69,14 @@ const Dashboard: React.FC = () => {
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      await api.delete(`/foods/${id}`);
+
+      const filteredFoods = foods.filter(food => food.id !== id);
+      setFoods(filteredFoods);
+    } catch (err) {
+      console.log(err); /* eslint-disable-line */
+    }
   }
 
   function toggleModal(): void {
